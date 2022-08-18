3b7874a8f5e02ca2f317a5a257d2a40d559b3823
nephros <nemo@pgxperiiia10>
2022-08-18 12:19:00 +0200

    add external settings file

---
+++ qml/config/settings.js
@@ -0,0 +1,21 @@
+.pragma library
+var config = {
+    "sources": {
+        "os":  'file:///etc/sailfish-release',
+        "hw":  'file:///etc/hw-release',
+        "ssu": 'file:///etc/ssu/ssu.ini',
+        "pm":  'file:///etc/patchmanager2.conf'
+    },
+    "submit": {
+        "scheme":    'https',
+        "host":      'forum.sailfishos.org',
+        "uri":       '/new-topic?category_id=13'
+    },
+    "bugtemplate": {
+        "host": "forum.sailfishos.org",
+        "uri": "/c/13/show.json",
+        "category": "category.topic_template"
+    }
+}
+
+// vim: ft=javascript expandtab ts=4 sw=4 st=4
