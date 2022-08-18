88706df510d9945f54f79c82f1865da544f97a99
nephros <nemo@pgxperiiia10>
2022-08-18 12:18:15 +0200

    update OS version file

--- qml/config/sfos_versions.js
+++ qml/config/sfos_versions.js
@@ -1,19 +1,35 @@
+/*
+ * Some Sailfish OS properties.
+ *
+ * Versions are taken from the ones available on OBS.
+ *
+ */
+.pragma library
 var data = {
     "version": [
         "4.4.0.68",
         "4.4.0.64",
         "4.4.0.58",
-        "4.3.0.x",
-        "4.2.0.x",
-        "4.1.0.x",
-        "4.0.0.x",
-        "3.4.x",
-        "3.2.x"
+        "4.3.0.15",
+        "4.3.0.12",
+        "4.2",
+        "4.1",
+        "4.0",
+        "3.4",
+        "3.3",
+        "3.2",
+        "3.1",
+        "3.0",
+        "2.x series",
+        "1.1 series",
+        "1.0 series",
+        "other"
     ],
     "arch": [
         "arm",
         "aarch64",
-        "x86"
+        "x86",
+        "amd64 (inofficial)"
     ]
 }
 
