79c754e3649592989e70959934fd81a37d0be994
nephros <nemo@pgxperiiia10>
2022-08-26 11:27:33 +0200

    Persistence: revamp database schema
    Contributes-To: #14

--- qml/js/util.js
+++ qml/js/util.js
@@ -36,7 +36,7 @@ function defuscate(data) {
 
 
 function getDb() {
-    var db = LS.LocalStorage.openDatabaseSync("Storage", "0.1", "Persistent Storage", 10000);
+    var db = LS.LocalStorage.openDatabaseSync("Storage", "0.2", "Persistent Storage", 10000);
     if (!db_initialized) {
         db_initialized = true;
         initDb(db);
@@ -47,8 +47,8 @@ function initDb(db) {
     console.debug("Init DB");
     try {
         db.transaction(function(tx) {
-            tx.executeSql('CREATE TABLE IF NOT EXISTS SavedContent( id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, name TEXT, value TEXT)');
-            tx.executeSql('INSERT INTO SavedContent values (?,?,?) ', [ 0, 'Default', 'NULL' ]);
+            //tx.executeSql('CREATE TABLE IF NOT EXISTS SavedContent( id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, name TEXT, post TEXT)');
+            tx.executeSql('CREATE TABLE IF NOT EXISTS AutoSaved(post TEXT)');
         })
     } catch (err) {
         console.warn("Error creating or adding table in database: " + err);
@@ -57,7 +57,7 @@ function initDb(db) {
 function clearDb(db) {
     try {
         db.transaction(function(tx) {
-            tx.executeSql('DROP TABLE SavedContent');
+            tx.executeSql('DROP TABLE AutoSaved');
         })
     } catch (err) {
         console.warn("Error creating table in database: " + err);
@@ -72,11 +72,11 @@ function dropDb(db) {
         console.warn("Error creating table in database: " + err);
     }}
 
-function resetDefault(){
+function resetAutoSaved(){
     var db = getDb();
     try {
         db.transaction( function(tx) {
-                var rs = tx.executeSql('DELETE FROM SavedContent where id = 0');
+                var rs = tx.executeSql('DROP TABLE AutoSaved');
         })
     } catch (err) {
         console.warn("Error executing transaction: " + err);
@@ -92,12 +92,13 @@ function load(){
     var ret = "";
     try {
         db.readTransaction(function(tx) {
-            var rs = tx.executeSql('SELECT value FROM SavedContent where id = 0');
-            var r = rs.rows.item(0).value;
+            var rs = tx.executeSql('SELECT post FROM AutoSaved LIMIT 1');
+            var r = rs.rows.item(0).post;
             ret = defuscate(r);
         })
     } catch (err) {
         console.warn("Error executing transaction: " + err);
+        return "";
     }
     return ret;
 }
@@ -112,7 +113,8 @@ function save(data){
     var db = getDb();
     try {
         db.transaction( function(tx) {
-            tx.executeSql('UPDATE SavedContent SET value = ? WHERE id = 0', [ odata ]);
+            tx.executeSql('DELETE from AutoSaved');
+            tx.executeSql('INSERT INTO AutoSaved VALUES(?)', [ odata ]);
         })
     } catch (err) {
         console.warn("Error executing transaction: " + err);
