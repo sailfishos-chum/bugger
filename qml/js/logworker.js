079644b250db189831bc5ad5f6536b76fb6b1cc9
nephros <nemo@pgxperiiia10>
2023-03-13 09:48:08 +0100

    record file sizes, warn about, remove empty files

--- qml/js/logworker.js
+++ qml/js/logworker.js
@@ -18,14 +18,10 @@ WorkerScript.onMessage = function(m) {
     /* load files from URLs into data buffer */
     function getFile(model, index) {
         var data = model.get(index)
-        //console.debug("Trying to load file contents for", JSON.stringify(data))
         var url = data.url
         var r = new XMLHttpRequest()
         r.open('GET', url);
         r.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
-        //r.responseType = 'text';
-        //r.responseType = 'arraybuffer'; //we need binary data
-        //r.responseType = 'blob'; //we need binary data
         r.send();
 
         r.onreadystatechange = function(event) {
@@ -34,10 +30,13 @@ WorkerScript.onMessage = function(m) {
                     console.debug("Filedata loaded: about", r.response.split("\n").length, "lines");
                     if (r.response.length > 0) {
                         model.setProperty(index, "dataStr", r.response)
-                        model.sync()
+                        // FIXME: use response header for this?
+                        model.setProperty(index, "fileSize", r.response.length)
                     } else {
-                        console.warn("File was empty, not added:", JSON.stringify(r.response));
+                        console.warn("File was empty, removing from list:", data.fileName);
+                        model.remove(index)
                     }
+                    model.sync()
                 } else {
                     console.warn("Filedata load failed:", JSON.stringify(r.response));
                 }
@@ -47,16 +46,13 @@ WorkerScript.onMessage = function(m) {
 
     function pasteFile(model, index, url, expire) {
         var data = model.get(index)
-        //console.debug("trying to upload:", JSON.stringify(data))
-        //uploading = data.fileName
         WorkerScript.sendMessage({ event: "uploading",  file: data.fileName})
         var r = new XMLHttpRequest()
         r.open('POST', url);
         r.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
         r.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-        //console.debug("Sending:", payload);
         const fileContent = data["dataStr"]
-        console.assert( fileContent.length>0, "Trying to upload empty log data")
+        console.assert( (fileContent.length>0), "Trying to upload empty log data")
 
         const payload =
             'expiry_days=' + expire
@@ -72,7 +68,6 @@ WorkerScript.onMessage = function(m) {
                     WorkerScript.sendMessage({ event: "pasteOk"})
                 } else {
                     console.warn("error in processing request.", r.status, r.statusText);
-                    //app.popup(qsTr("Error uploading: %1 - %2", "%1: error code, %2: error message").arg(r.status).arg(r.statusText));
                     WorkerScript.sendMessage({ event: "pasteError", code: r.status, text: r.statusText })
                 }
             }
