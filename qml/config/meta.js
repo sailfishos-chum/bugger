3757c4bebda5a635e3c0e00b8d5762c20397a7d3
nephros <nemo@pgxperiiia10>
2023-03-20 09:51:09 +0100

    Add Categories selector
    Contributes-To: #40

--- qml/config/meta.js
+++ qml/config/meta.js
@@ -1,24 +1,64 @@
 .pragma library
+/*
+ * When adding a category, add it at the end!!
+ *
+ */
 var data = {
-	"categories": [
-		{ "id": 0, "name": "none",	"displayName": "Not specified", "desc": ""},
-		{ "id": 0, "name": "android_app_support",	"displayName": "Android App Support", "desc": ""},
-		{ "id": 0, "name": "backup",		"displayName": "Backup", "desc": ""},
-		{ "id": 0, "name": "bluetooth",	"displayName": "Bluetooth", "desc": ""},
-		{ "id": 0, "name": "browser",		"displayName": "Browser", "desc": ""},
-		{ "id": 0, "name": "calendar",	"displayName": "Calendar", "desc": ""},
-		{ "id": 0, "name": "contacts",	"displayName": "Contacts", "desc": ""},
-		{ "id": 0, "name": "exchange",	"displayName": "Exchange/Office365", "desc": ""},
-		{ "id": 0, "name": "fingerprint",	"displayName": "Fingerprint", "desc": ""},
-		{ "id": 0, "name": "mail",		"displayName": "EMail", "desc": ""},
-		{ "id": 0, "name": "media_tracker",	"displayName": "Media Indexing", "desc": ""},
-		{ "id": 0, "name": "mobile_data",	"displayName": "Mobile Data", "desc": ""},
-		{ "id": 0, "name": "mpris",		"displayName": "MPRIS", "desc": ""},
-		{ "id": 0, "name": "native app",	"displayName": "SFOS App", "desc": ""},
-		{ "id": 0, "name": "network",		"displayName": "Networking", "desc": ""},
-		{ "id": 0, "name": "ngfd",		"displayName": "UI Feedback", "desc": ""},
-		{ "id": 0, "name": "oom_killer",	"displayName": "OOM", "desc": ""},
-		{ "id": 0, "name": "positioning",	"displayName": "Positionig", "desc": ""},
-	]
+    "categories": [
+        { "id": 0,  "name": "none",                 "displayName": "None",
+                "description": "No specific category"
+        },
+        { "id": 1,  "name": "android_app_support",  "displayName": "Android App Support",
+                "description": ""
+        },
+        { "id": 2,  "name": "backup",               "displayName": "Backup",
+                "description": "The Sailfish Backup system"
+        },
+        { "id": 3,  "name": "bluetooth",            "displayName": "Bluetooth",
+                "description": "BT connection or devices"
+        },
+        { "id": 4,  "name": "browser",              "displayName": "Browser",
+                "description": "Web browsing"
+        },
+        { "id": 5,  "name": "calendar",             "displayName": "Calendar",
+                "description": "Calendar sync or management problems"
+        },
+        { "id": 6,  "name": "contacts",             "displayName": "Contacts",
+                "description": "Contact sync or management problems"
+        },
+        { "id": 7,  "name": "exchange",             "displayName": "Exchange",
+                "description": "Exchange/O356 sync or management problems"
+        },
+        { "id": 8,  "name": "fingerprint",          "displayName": "Fingerprint",
+                "description": "Fingerprint Reader"
+        },
+        { "id": 9,  "name": "mail",                 "displayName": "EMail",
+                "description": "General EMail"
+        },
+        { "id": 10, "name": "media_tracker",        "displayName": "Media Indexing",
+                "description": "Media Player remote control (MPRIS)"
+        },
+        { "id": 11, "name": "mobile_data",          "displayName": "Mobile Data",
+                "description": ""
+        },
+        { "id": 12, "name": "mpris",                "displayName": "MPRIS",
+                "description": ""
+        },
+        { "id": 13, "name": "native_app",           "displayName": "SFOS App",
+                "description": "Sailfish stock apps"
+        },
+        { "id": 14, "name": "network",              "displayName": "Networking",
+                "description": ""
+        },
+        { "id": 15, "name": "ngfd",                 "displayName": "UI Feedback",
+                "description": "Non-graphical feedback"
+        },
+        { "id": 16, "name": "oom_killer",           "displayName": "OOM",
+                "description": "Out-of-memory conditions"
+        },
+        { "id": 17, "name": "positioning",          "displayName": "Positioning",
+                "description": "GPS and other positioning methods"
+        },
+    ]
 }
 // vim: ft=javascript expandtab ts=4 sw=4 st=4
