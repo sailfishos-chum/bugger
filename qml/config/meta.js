8c5a54484ff667c81e984e3e58479ae747f73bb4
nephros (PineTab) <sailfish@nephros.org>
2023-03-20 16:11:31 +0100

    remove id from metatags

--- qml/config/meta.js
+++ qml/config/meta.js
@@ -70,58 +70,58 @@ var data = {
  */
 var data = {
     "categories": [
-        { "id": 0,  "name": "none",                 "displayName": "None",
+        {  "name": "none", "displayName": "None",
                 "description": "No specific category"
         },
-        { "id": 1,  "name": "android_app_support",  "displayName": "Android App Support",
+        {  "name": "android_app_support",  "displayName": "Android App Support",
                 "description": ""
         },
-        { "id": 2,  "name": "backup",               "displayName": "Backup",
+        {  "name": "backup",   "displayName": "Backup",
                 "description": "The Sailfish Backup system"
         },
-        { "id": 3,  "name": "bluetooth",            "displayName": "Bluetooth",
-                "description": "BT connection or devices"
+        {  "name": "bluetooth", "displayName": "Bluetooth",
+                "description": "Bluetooth connection or devices"
         },
-        { "id": 4,  "name": "browser",              "displayName": "Browser",
+        {  "name": "browser",  "displayName": "Browser",
                 "description": "Web browsing"
         },
-        { "id": 5,  "name": "calendar",             "displayName": "Calendar",
+        {  "name": "calendar", "displayName": "Calendar",
                 "description": "Calendar sync or management problems"
         },
-        { "id": 6,  "name": "contacts",             "displayName": "Contacts",
+        {  "name": "contacts", "displayName": "Contacts",
                 "description": "Contact sync or management problems"
         },
-        { "id": 7,  "name": "exchange",             "displayName": "Exchange",
+        {  "name": "exchange", "displayName": "Exchange",
                 "description": "Exchange/O356 sync or management problems"
         },
-        { "id": 8,  "name": "fingerprint",          "displayName": "Fingerprint",
+        {  "name": "fingerprint",  "displayName": "Fingerprint",
                 "description": "Fingerprint Reader"
         },
-        { "id": 9,  "name": "mail",                 "displayName": "EMail",
+        {  "name": "mail", "displayName": "EMail",
                 "description": "General EMail"
         },
-        { "id": 10, "name": "media_tracker",        "displayName": "Media Indexing",
+        { "name": "media_tracker", "displayName": "Media Indexing",
                 "description": "Media Player remote control (MPRIS)"
         },
-        { "id": 11, "name": "mobile_data",          "displayName": "Mobile Data",
+        { "name": "mobile_data",  "displayName": "Mobile Data",
                 "description": ""
         },
-        { "id": 12, "name": "mpris",                "displayName": "MPRIS",
+        { "name": "mpris", "displayName": "MPRIS",
                 "description": ""
         },
-        { "id": 13, "name": "native_app",           "displayName": "SFOS App",
+        { "name": "native_app",   "displayName": "SFOS App",
                 "description": "Sailfish stock apps"
         },
-        { "id": 14, "name": "network",              "displayName": "Networking",
+        { "name": "network",  "displayName": "Networking",
                 "description": ""
         },
-        { "id": 15, "name": "ngfd",                 "displayName": "UI Feedback",
+        { "name": "ngfd", "displayName": "UI Feedback",
                 "description": "Non-graphical feedback"
         },
-        { "id": 16, "name": "oom_killer",           "displayName": "OOM",
+        { "name": "oom_killer",   "displayName": "OOM",
                 "description": "Out-of-memory conditions"
         },
-        { "id": 17, "name": "positioning",          "displayName": "Positioning",
+        { "name": "positioning",  "displayName": "Positioning",
                 "description": "GPS and other positioning methods"
         },
     ]
