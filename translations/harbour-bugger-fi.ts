3cf4530ecdc7e8d9c58e77d95cced295cfe7ddd6
Elmeri Länsiharju <139926936+tuplasuhveli@users.noreply.github.com>
2026-03-10 12:24:51 +0200

    Fix XML declaration in harbour-bugger-fi.ts

--- translations/harbour-bugger-fi.ts
+++ translations/harbour-bugger-fi.ts
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="fi" sourcelanguage="fi">
+<TS version="2.1" language="fi">
 <context>
     <name></name>
     <message id="harbour-bugger-qa_name">
